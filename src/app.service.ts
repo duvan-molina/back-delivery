4f27db6e547037e4bb7178aa9444a2a4356121ec
Duvan Molina <duvanmoliina@gmail.com>
2023-04-02 09:10:01 -0500

    upload files

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { uploadImage } from './helpers/cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+  }
+  async uploadFiles(files: Array<Express.Multer.File>) {
+    const urls = [];
+    for (const file of files) {
+      const res = await uploadImage(file);
+      // @ts-ignore
+      urls.push(res.secure_url);
+    }
+    return { urls };
   }
 }
